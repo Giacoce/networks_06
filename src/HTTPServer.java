7fd15ea8b24a1fb3b9e8ba73b0e0b05a5e10dd0f
giacoce <giacomo.corbett---englaro@city.ac.uk>
2026-03-25 12:10:33 +0000

    yoooo

--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -9,15 +9,17 @@ public class HTTPServer {
         System.out.println("Starting server on port " + port);
         ServerSocket serverSocket = new ServerSocket(port);
 
-        System.out.println("Waiting for a client...");
-        while(true) {
-
-
+        while (true) {
+            System.out.println("Waiting for a client...");
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected");
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream())
+            );
+            OutputStreamWriter writer = new OutputStreamWriter(
+                    clientSocket.getOutputStream()
+            );
 
             String firstLine = reader.readLine();
             System.out.println("Request: " + firstLine);
@@ -27,44 +29,46 @@ public class HTTPServer {
                 System.out.println("Header: " + line);
             }
 
+            String body;
+            String statusLine;
+
             if (firstLine != null) {
                 String[] requestParts = firstLine.split(" ");
 
-                String method = requestParts[0];
-                String path = requestParts[1];
+                if (requestParts.length >= 2) {
+                    String method = requestParts[0];
+                    String path = requestParts[1];
 
-                String headerLine;
-                while ((headerLine = reader.readLine()) != null && !headerLine.equals("")) {
-                    System.out.println(headerLine);
-                }
-
-
-                String body;
-                String statusLine;
-
-                if (method.equals("GET")) {
-                    if (path.equals("/") || path.equals("/index.html")) {
-                        statusLine = "HTTP/1.1 200 OK\r\n";
-                        body = "<html><body><h1>Welcome</h1><p>This is my page.</p></body></html>";
+                    if (method.equals("GET")) {
+                        if (path.equals("/") || path.equals("/index.html")) {
+                            statusLine = "HTTP/1.1 200 OK\r\n";
+                            body = "<html><body><h1>Welcome</h1><p>This is my page.</p></body></html>";
+                        } else {
+                            statusLine = "HTTP/1.1 400 Bad Request\r\n";
+                            body = "<html><body><h1>400 Bad Request</h1><p>Page not found.</p></body></html>";
+                        }
                     } else {
                         statusLine = "HTTP/1.1 400 Bad Request\r\n";
-                        body = "<html><body><h1>400 Bad Request</h1><p>Page not found.</p></body></html>";
+                        body = "<html><body><h1>400 Bad Request</h1><p>Only GET is supported.</p></body></html>";
                     }
                 } else {
                     statusLine = "HTTP/1.1 400 Bad Request\r\n";
-                    body = "<html><body><h1>400 Bad Request</h1><p>Only GET is supported.</p></body></html>";
+                    body = "<html><body><h1>400 Bad Request</h1><p>Malformed request.</p></body></html>";
                 }
-
-                writer.write(statusLine);
-                writer.write("Content-Type: text/html\r\n");
-                writer.write("Content-Length: " + body.length() + "\r\n");
-                writer.write("\r\n");
-                writer.write(body);
-                writer.flush();
+            } else {
+                statusLine = "HTTP/1.1 400 Bad Request\r\n";
+                body = "<html><body><h1>400 Bad Request</h1><p>Empty request.</p></body></html>";
             }
 
+            writer.write(statusLine);
+            writer.write("Content-Type: text/html\r\n");
+            writer.write("Content-Length: " + body.length() + "\r\n");
+            writer.write("Connection: close\r\n");
+            writer.write("\r\n");
+            writer.write(body);
+            writer.flush();
+
             clientSocket.close();
-            serverSocket.close();
         }
     }
 }
